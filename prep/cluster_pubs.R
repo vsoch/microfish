d7102825a1583983ac7b3d2f1c7df58be18b92fc
vsoch <vsochat@stanford.edu>
2016-05-15 11:54:56 -0700

    adding basic author view page

--- prep/cluster_pubs.R
+++ prep/cluster_pubs.R
@@ -1,8 +1,7 @@
 library(pheatmap)
 data = read.csv("microsoft_pubs.tsv",sep="\t",row.names=1)
+data[is.null(data)] = 0
+data[is.na(data)] = 0
 pdf("microsoft_pubs.pdf",width=30,height=20)
 pheatmap(data)
 dev.off()
-
-# Save clustering for plotting ourselves
-hm = pheatmap(data)
